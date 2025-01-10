4b973ad191754f651a2c24f4896103f83c2c9b1f
Ahmad <ahmad@gmail.com>
2025-01-10 11:46:05 +0100

    Début de la structure MVC
    Début de la structure MVC

--- app/Controllers/Moncontroleur.php
+++ app/Controllers/Moncontroleur.php
@@ -5,8 +5,17 @@ class Moncontroleur extends BaseController{
 
     public function index() {
         //Retourne la vue d'accueil dans le controleur
-       return view('v_Accueil.php');
+       //return view('v_Accueil.php');
        //return view('v_Connexion.php');
+       $_REQUEST['uc'] = "accueil";
+
+       $uc = $_REQUEST['uc'];
+
+       switch($uc) {
+            case 'accueil':{
+                return view('v_Accueil.php');
+            }
+       }
     }
 }
 
